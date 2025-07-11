122ad639d91dc600883db785ebd3ddfbaf923ec8
Karthikeyan Prabakaran <145884650+karthiprabha-b@users.noreply.github.com>
2025-07-11 13:05:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Install Playwright Chromium (needed for scraping)
 python -m playwright install chromium
+
+# Start the Flask app using Gunicorn
 gunicorn app:app --bind 0.0.0.0:8080
